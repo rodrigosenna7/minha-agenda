4afb214145d80c0d3403aad47f01c85977f9a9cf
rodrigosenna7 <rsena671@gmail.com>
2021-06-07 13:02:46 -0300

    arquivos iniciais

---
+++ src/app/app.module.ts
@@ -0,0 +1,64 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { MeetingListComponent } from './components/meeting-list/meeting-list.component';
+import { MeetingFormComponent } from './components/meeting-form/meeting-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { DeleteComponent } from './components/delete/delete.component';
+import { LocalDateTimePipe } from './pipe/local-date-time.pipe';
+
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+
+
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatSelectModule} from '@angular/material/select';
+import { ViewComponent } from './components/view/view.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MeetingListComponent,
+    MeetingFormComponent,
+    DeleteComponent,
+    LocalDateTimePipe,
+    ViewComponent
+  ],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatDialogModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatPaginatorModule,
+    MatSelectModule
+  ],
+  providers: [
+    LocalDateTimePipe
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/service/meeting.service.ts
@@ -0,0 +1,61 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MeetingService {
+
+
+  MOBILE_BAAS_URL: string = 'https://mobilebaas.com/backend/api/manage/db';
+  tableName:string = 'meeting';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'MOBILEBAASKEY': 'MTYyMzAxOTY5MzE0OFJPRFJJR08gU0VOQSBERSBNQVRPU3Nz'
+    })
+  };
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  insert(meeting: any) {
+    return this.http.post(this.MOBILE_BAAS_URL+'?table='+this.tableName,meeting,this.httpOptions);
+  }
+
+  update(meeting: any) {
+    return this.http.put(this.MOBILE_BAAS_URL+'?table='+this.tableName,meeting,this.httpOptions);
+  }
+
+  delete(id: string) {
+    return this.http.delete(this.MOBILE_BAAS_URL+'/'+id+'?table='+this.tableName,this.httpOptions);
+  }
+
+  getById(id: string) {
+    return this.http.get(this.MOBILE_BAAS_URL+'/'+id+'?table='+this.tableName,this.httpOptions);
+  }
+
+  getAll(pageNumber: number,totalRecordsPerPage: number, sortField:string,filters:string) {
+    let parameters = '?table='+this.tableName;
+
+    if(pageNumber != null){
+        parameters += '&pageNumber='+pageNumber;
+    }
+
+    if(totalRecordsPerPage != null){
+        parameters += '&totalRecordsPerPage='+totalRecordsPerPage;
+    }
+
+    if(sortField != null){
+        parameters += '&sortField='+sortField;
+    }
+    
+    if(filters != null){
+      parameters+= '&filters='+filters;
+    }
+    return this.http.get(this.MOBILE_BAAS_URL+'/find'+parameters,this.httpOptions);
+  }
+
+}
